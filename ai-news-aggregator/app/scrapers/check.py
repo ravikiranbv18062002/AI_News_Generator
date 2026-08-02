dc919a90ff25431f9984a248bcca654a4ae189b2
Ravikiran Vasanthakumar <ravikiran.v@ascendion.com>
2026-08-02 23:22:54 +0530

    check.py

--- ai-news-aggregator/app/scrapers/check.py
+++ ai-news-aggregator/app/scrapers/check.py
@@ -1,1 +1,3 @@
 https://github.com/datalumina/ai-news-aggregator/blob/deployment-final/app/agent/curator_agent.py
+
+https://takeuforward.org/system-design/complete-system-design-roadmap-with-videos-for-sdes
